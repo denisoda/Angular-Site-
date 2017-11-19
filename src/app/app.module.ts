243a4cb8798d26f1fddeb776eba1d08c58bd70a3
Ilya Dzeraziak <ilya.by@bk.ru>
2017-11-19 23:01:06 +0300

    Declaration of new class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MyInfoComponent } from './my-info/my-info.component';
-
+import {MyInfo} from './my-info';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyInfoComponent
+    MyInfoComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
